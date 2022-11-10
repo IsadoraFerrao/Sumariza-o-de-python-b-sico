3e85bd286f4349f0d484e73e7afd9d0cadd56cfe
Isadora <isadora-gf@hotmail.com>
2022-11-10 17:54:42 -0300

    several examples of python

---
+++ exemplo_funcao.py
@@ -0,0 +1,8 @@
+#funcoes
+
+def apresentacao(nome, idade):
+    print(f'Ola, meu nome é {nome} e minha idade e {idade}')
+
+nome = input('Digite seu nome: ')
+idade = 25
+apresentacao(nome, idade) #invocacao de funcao
